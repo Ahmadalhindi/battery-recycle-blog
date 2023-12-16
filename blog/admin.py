368f73934a953bb1a3ab62c69bc29938910ba3d7
Ahmadalhindi <>
2023-12-16 19:01:05 +0000

    Register comment in admin panel and add approve_comment method and adjust the display for blog post using Summernote.

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'categories', 'created_at')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'post', 'created_at', 'approved')
+    list_filter = ('approved', 'created_at')
+    search_fields = ('user', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -49,7 +49,7 @@ class Post(models.Model):
         return self.likes.count()
 
 
-class comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
